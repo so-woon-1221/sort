d46dbb4b618072c0281cb3d01aa1f9c0e7eb366b
sowoon <janus7359@gmail.com>
2020-05-03 23:47:50 +0900

    백트래킹 15651

--- src/backtracking2.java
+++ src/backtracking2.java
@@ -1,26 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class backtracking2 {
     static int n;
     static int m;
     static int[] ans;
+    static String ans2;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        String prob = br.readLine();
+        StringTokenizer token = new StringTokenizer(prob);
+        n = Integer.parseInt(token.nextToken());
+        m = Integer.parseInt(token.nextToken());
         ans = new int[m];
         backtracking2 bc = new backtracking2();
 
-        for (int i = 1; i <= n; i++) {
-            ans[0] = i;
-
-            bc.godown(1);
-        }
+        bc.godown(0);
     }
 
-    void godown(int row) {
+    void godown(int row) throws IOException {
         if (row < m) {
             for (int i = 1; i <= n; i++) {
                 ans[row] = i;
@@ -28,9 +29,10 @@ public class backtracking2 {
             }
         } else {
             for (int i = 0; i < m; i++) {
-                System.out.print(ans[i] + " ");
+                bw.write(Integer.toString(ans[i])+" ");
             }
-            System.out.println();
+            bw.flush();
+            bw.newLine();
         }
     }
 }
