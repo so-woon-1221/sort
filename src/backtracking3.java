e7f6f07a81da7ed1ea452cc12ce016364b02eeb4
sowoon <janus7359@gmail.com>
2020-05-03 23:50:43 +0900

    백트래킹 15652

---
+++ src/backtracking3.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class backtracking3 {
+    static int n;
+    static int m;
+    static int[] ans;
+    static String ans2;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+        String prob = br.readLine();
+        StringTokenizer token = new StringTokenizer(prob);
+        n = Integer.parseInt(token.nextToken());
+        m = Integer.parseInt(token.nextToken());
+        ans = new int[m];
+        backtracking3 bc = new backtracking3();
+
+        bc.godown(0);
+        br.close();
+        bw.close();
+    }
+
+    void godown(int row) throws IOException {
+        if (row < m) {
+            for (int i = 1; i <= n; i++) {
+                int count = 0;
+                for (int j = 0; j < row; j++) {
+                    if (ans[j] > i)
+                        count++;
+                }
+                if (count == 0) {
+                    ans[row] = i;
+                    godown(row + 1);
+                }
+            }
+        } else {
+            for (int i = 0; i < m; i++) {
+                bw.write(Integer.toString(ans[i]) + " ");
+            }
+            bw.flush();
+            bw.newLine();
+        }
+    }
+}
